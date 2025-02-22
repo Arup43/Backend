5e94e02449e0079b090ee541acebd799b0bbf135
Arup43 <debnatharupbzs@gmail.com>
2025-02-23 01:35:25 +0600

    fix

--- src/main/java/com/explore/backend/config/RabbitMQConfig.java
+++ src/main/java/com/explore/backend/config/RabbitMQConfig.java
@@ -2,14 +2,30 @@ package com.explore.backend.config;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 
 @Configuration
 public class RabbitMQConfig {
+
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+
+    @Value("${spring.rabbitmq.port}")
+    private int port;
+
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
 
     public static final String COMMAND_QUEUE = "command_queue";
 
@@ -17,6 +33,21 @@ public class RabbitMQConfig {
     public Queue commandQueue() {
         return QueueBuilder.durable(COMMAND_QUEUE)
                 .build();
+    }
+
+    @Bean
+    public CachingConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setHost(host);
+        connectionFactory.setPort(port);
+        connectionFactory.setUsername(username);
+        connectionFactory.setPassword(password);
+        try {
+            connectionFactory.getRabbitConnectionFactory().useSslProtocol();
+        } catch (KeyManagementException | NoSuchAlgorithmException e) {
+            throw new RuntimeException("SSL Protocol initialization failed", e);
+        }
+        return connectionFactory;
     }
 
     @Bean
